e6612c503b6d47e391fecb2c08761056af21668e
Fady Moheb <92612410+N1NJ10@users.noreply.github.com>
2024-01-17 15:31:00 +0200

    Update posts.js

--- scripts/posts/posts.js
+++ scripts/posts/posts.js
@@ -3,13 +3,8 @@ import Broker from "./post/Broker.js"
 import Authority from "./post/Authority.js"
 import Active from "./post/Active.js"
 import ECPPT from "./post/Ecppt_The_honest.js"
-import auto from "./post/Auto_Login.js"
 import F5 from "./post/F5.js"
-import ntlm from "./post/ntlm.js"
 import SQL from "./post/SQL.js"
-import poison from "./post/poison.js"  
-import arp from "./post/arp.js"
-
 const posts = [
     Macros,
     Broker,
