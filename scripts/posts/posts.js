da6cf2e0dc02882785d843311ba5b05fb3f6d313
Fady Moheb <92612410+N1NJ10@users.noreply.github.com>
2024-01-23 08:24:00 +0200

    Update posts.js

--- scripts/posts/posts.js
+++ scripts/posts/posts.js
@@ -1,12 +1,25 @@
 import Macros from "./post/Macros.js"
+import Authority from "./post/Authority.js"
+import ECPPT from "./post/Ecppt_The_honest.js"
+import F5 from "./post/F5.js"
+import Broker from "./post/Broker.js"
+import SQL from "./post/SQL.js"
+import ntlm from "./post/ntlm.js"
+import poison from "./post/poison.js"
 
 const posts = [
-    Macros
-    ];
-
+Macros,
+Authority,
+ECPPT,
+F5,
+poison,
+ntlm,
+Broker,
+SQL,
+];
 
 for (let i = 0; i < posts.length; i++) {
-    posts[i].id = i;
+posts[i].id = i;
 }
 
 export default posts;
